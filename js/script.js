eff9bb214e2c8a78b50dc2f7b21833e6a488eaa7
haditariq <hadi-tariq@live.co.uk>
2017-03-11 00:53:22 +0500

    testing

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//
+//$('.headnav').ready(function(){
+//    $('nav ul').toggleClass('.showing');
+//});
+//
+$(window).ready(function(){
+$('.temp').ready(function(){
+    $(this).mouseenter('hello');
+});
+});
---
+++ skills-part.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Home</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css">
+<style>
+  
+   
+    </style>
+</head>
+ <body >
+<!--             staring-->
+  <div class="col-lg-9 col-md-9 col-sm-12 col-sx-12 skills-pad">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+          <img class="skills-image" src="http://www.w3schools.com/css/trolltunga.jpg" alt="skill"  />
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+           <div class=" skills-btn-decor"> Add</div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+          <div class="skills-btn-decor-01"> Modify</div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+          <div class="skills-btn-decor-02"> Delete</div>
+      </div>
+      
+  </div>
+<!--                     ending-->
+    <div class="col-lg-9 col-md-9 col-sm-12 col-sx-12 skills-pad">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+          <img class="skills-image" src="http://www.w3schools.com/css/trolltunga.jpg" alt="skill"  />
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+           <div class=" skills-btn-decor"> Add</div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+          <div class="skills-btn-decor-01"> Modify</div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+          <div class="skills-btn-decor-02"> Delete</div>
+      </div>
+      
+  </div>
+    <div class="col-lg-9 col-md-9 col-sm-12 col-sx-12 skills-pad">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+          <img class="skills-image" src="http://www.w3schools.com/css/trolltunga.jpg" alt="skill"  />
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+           <div class=" skills-btn-decor"> Add</div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+          <div class="skills-btn-decor-01"> Modify</div>
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 skills-btn-height">
+          <div class="skills-btn-decor-02"> Delete</div>
+      </div>
+      
+  </div>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+
+</html>
